474cbcdac458c60ae034f6f71beefa05de6e2d01
Aditya Kumar Sharma <adityakd171@gmail.com>
2022-09-05 11:54:44 +0530

    added data of various sizes

--- client/formData.js
+++ client/formData.js
@@ -15,3 +15,91 @@ btn.onclick = display();
 
 victimName.value="aditya";
 console.log(psName);
+const newWarrant = {
+    warrant :[
+        {
+            policeStation: "Central Police Station",
+            onDutyOfficer: "Ajay Singh",
+            date: "12-31-2020",
+            victimName: "Harish",
+            accusedName: "Sanjay",
+            summary: "Lorem simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sLorem  's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and ",
+            fileHash: "Qmc5KovUi3gEdxPckbLp92i7qKN5TGQe6fJJb32M6JogBC"
+          },
+          {
+            policeStation: "Central Police Station",
+            onDutyOfficer: "Ajay Singh",
+            date: "12-31-2020",
+            victimName: "Harish",
+            accusedName: "Sanjay",
+            summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's  unknownunknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            fileHash: "Qmc5KovUi3gEdxPckbLp92i7qKN5TGQe6fJJb32M6JogBC"
+          },
+          
+      {
+        policeStation: "Central Police Station",
+        onDutyOfficer: "Ajay Singh",
+        date: "12-31-2020",
+        victimName: "Harish",
+        accusedName: "Sanjay",
+        summary: "Lorem Ipsum is a text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy textab ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        fileHash: "Qmc5KovUi3gEdxPckbLp92i7qKN5TGQe6fJJb32M6JogBC"
+      },
+      {
+        policeStation: "Central Police Station",
+        onDutyOfficer: "Ajay Singh",
+        date: "12-31-2020",
+        victimName: "Harish",
+        accusedName: "Sanjay Singh",
+        summary: "setting industry. Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        fileHash: "Qmc5KovUi3gEdxPckbLp92i7qKN5TGQe6fJJb32M6JogBC"
+      },
+      {
+        policeStation: "Central Police Station",
+        onDutyOfficer: "Ajay Singh",
+        date: "12-31-2020",
+        victimName: "Harish",
+        accusedName: "Sanjay",
+        summary: "when unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        fileHash: "Qmc5KovUi3gEdxPckbLp92i7qKN5TGQe6fJJb32M6JogBC"
+      },
+      {
+        policeStation: "Central Police Station",
+        onDutyOfficer: "Ajay Singh",
+        date: "12-31-2020",
+        victimName: "Harish",
+        accusedName: "Sanjay",
+        summary: "specimenbook",
+        fileHash: "Qmc5KovUi3gEdxPckbLp92i7qKN5TGQe6fJJb32M6JogBC"
+      },{
+        policeStation: "Central Police Station",
+        onDutyOfficer: "",
+        date: "",
+        victimName: "Harish",
+        accusedName: "Sanjay",
+        summary: ",book.",
+        fileHash: "Qmc5KovUi3gEdxPckbLp92i7qKN5TGQe6fJJb32M6JogBC"
+      },
+      
+      {
+        policeStation: "Police Stan",
+        onDutyOfficer: "",
+        date: "",
+        victimName: "",
+        accusedName: "",
+        summary: ",  .",
+        fileHash: "Qmc5KovUi3gEdxPckbLp92i7qKN5TGQe6fJJb32M6JogBC"
+      },
+      {
+        policeStation: "",
+        onDutyOfficer: "",
+        date: "",
+        victimName: "",
+        accusedName: "",
+        summary: "",
+        fileHash: "Qmc5KovUi3gEdxPckbLp92i7qKN5TGQe6fJJb32M6JogBC"
+      },
+      
+    ]
+    
+  }
